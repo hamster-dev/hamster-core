598ab5d042bf4e61171190204886a2c3c6706cc4
Gregory Armer <greg@sigterm.sh>
2014-03-29 10:36:35 -0400

    Auto provision rabbitmq admin UI

--- puppet/modules/rabbitmq/manifests/init.pp
+++ puppet/modules/rabbitmq/manifests/init.pp
@@ -4,6 +4,12 @@ class rabbitmq {
     service { 'rabbitmq-server':
         ensure    => true,
         enable    => true,
-        subscribe => [ Package[rabbitmq-server] ],
+        subscribe => [ Package[rabbitmq-server], Exec[enable_rabbitmq_management] ],
+    }
+
+    exec { 'enable_rabbitmq_management':
+        command     => "/usr/sbin/rabbitmq-plugins enable rabbitmq_management",
+        environment => "HOME=/root",
+        unless      => "/bin/grep rabbitmq_management /etc/rabbitmq/enabled_plugins",
     }
 }
