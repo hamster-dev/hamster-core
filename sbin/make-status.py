1ab20d27cb6a54d26a584a099c5ec92dd326b459
Mike Waters <robert.waters@dealertrack.com>
2015-09-11 19:38:16 -0400

    Modified make-status.py to use hamster env vars if they exist.

--- sbin/make-status.py
+++ sbin/make-status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Makes a test commit status on the most recent commit of a repo.
-
+import os
 from functools import partial
 # requires github3.py==1.0.0a2
 from github3 import GitHub, GitHubEnterprise
@@ -10,9 +10,9 @@ from github3 import GitHub, GitHubEnterprise
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', dest='user', required=True)
-parser.add_argument('-t', dest='token', required=True)
-parser.add_argument('-g', dest='github', default='github.com')
+parser.add_argument('-u', dest='user', default=os.environ.get('HAMSTER_GITHUB_USER'))
+parser.add_argument('-t', dest='token', default=os.environ.get('HAMSTER_GITHUB_API_TOKEN'))
+parser.add_argument('-g', dest='github', default=os.environ.get('HAMSTER_GITHUB_HOST', 'github.com'))
 parser.add_argument('-o', dest='owner', required=True)
 parser.add_argument('-r', dest='repo', required=True)
 parser.add_argument('--context', default='hamster')
@@ -23,6 +23,8 @@ parser.add_argument('--state', default='success')
 args = parser.parse_args()
 
 assert args.state in ('pending', 'success', 'error', 'failure')
+assert args.user
+assert args.token
 
 if args.github == 'github.com':
     gh = GitHub
