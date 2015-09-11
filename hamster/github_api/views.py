523076287f22dca0c1dd9f336fea73eeb6faaedc
Mike Waters <robert.waters@dealertrack.com>
2015-09-11 19:25:42 -0400

    Removed old debugging code from github api views.

--- hamster/github_api/views.py
+++ hamster/github_api/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
 
-from pipeline.executor import Executor
-
-import logging
+from pipeline.pipeline import Pipeline
 from pipeline.actions import TaskAction
 
+import logging
 logger = logging.getLogger(__name__)
 
 
@@ -13,35 +12,5 @@ logger = logging.getLogger(__name__)
 def debug_view(request):
     """Debug view for local testing.
     """
-    dct = {
-        'task': 'print_something',
-        'kwargs': {
-            'the_thing': 'came from debug_view'
-        },
-        'handlers': [
-            {
-                'task': 'print_something',
-                'kwargs': {
-                    'the_thing': 'this is a handler!'
-                },
-                'predicate': "not parent",
-                'handlers': [
-                    {
-                        'task': 'print_something',
-                        'kwargs': {
-                            'the_thing': 'INCEPTION!'
-                        },
-                    },
-                ]
-
-            },
-        ]
-    }
-    actions = [TaskAction.from_dict(dct)]
-    executor = Executor()
-
-    count = int(request.GET.get('count', 1))
-    for i in range(count):
-        executor.schedule(actions, None)
 
     return HttpResponse()
